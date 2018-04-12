6a0c3e57ccedca821cd0ced1fd9e5d6d312ad4ca
KomodoVu <37296951+KomodoVu@users.noreply.github.com>
2018-04-12 22:27:43 +0700

    get rid of redundant code

--- js/Sliding Animation.js
+++ js/Sliding Animation.js
@@ -16,13 +16,6 @@ function jumpIn() {
 	});
 }
 
-function jumpInNoCallBack() {
-    popUp.animate({
-        top: '90%',
-        opacity: '1'
-    }, 700);
-}
-
 function fadeOut() {
     popUp.animate({
         opacity: '0'
