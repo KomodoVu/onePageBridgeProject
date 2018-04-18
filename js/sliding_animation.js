47661ca60ac51fd6cc05f45baff4293607ef133e
KomodoVu <37296951+KomodoVu@users.noreply.github.com>
2018-04-18 21:36:03 +0700

    added indicators for the big slider, fix some small bug, finish footer except for the form validation

--- js/sliding_animation.js
+++ js/sliding_animation.js
@@ -8,6 +8,12 @@ var slider = $('#slider');
 var buttonNext = $('.button_next');
 var buttonPrev = $('.button_prev');
 var popUp = $('.pop_up_big');
+var indcator = $('.indicator');
+var indcator1 = $('#indicator1');
+var indcator2 = $('#indicator2');
+var indcator3 = $('#indicator3');
+var whatIndicator = 0;
+var disableIndicator = false;
 
 setTimeout(
   function() {
@@ -15,6 +21,59 @@ setTimeout(
   }, 1500);
 $('#slider .slider_section:last').insertBefore('#slider .slider_section:first');
 slider.css('margin-left', '-100%');
+indcator.eq(whatIndicator).addClass("active");
+
+function nextActive(){
+	if(whatIndicator == 2){
+		indcator.removeClass("active");
+		whatIndicator = 0;
+		indcator.eq(whatIndicator).addClass("active");
+	}
+	else{
+		indcator.removeClass("active");
+		whatIndicator++;
+		indcator.eq(whatIndicator).addClass("active");
+	}
+}
+
+function prevActive(){
+	if(whatIndicator == 0){
+		indcator.removeClass("active");
+		whatIndicator = 2;
+		indcator.eq(whatIndicator).addClass("active");
+	}
+	else{
+		indcator.removeClass("active");
+		whatIndicator--;
+		indcator.eq(whatIndicator).addClass("active");
+	}
+}
+
+function fisrtToLast() {
+	$('#slider .slider_section:last').insertBefore('#slider .slider_section:first');
+	$('#slider .slider_section:last').insertAfter('#slider .slider_section:first');
+	slider.animate({
+		marginLeft: '-200%'
+	}, 700);
+	setTimeout(
+		function () {
+			$('#slider .slider_section:last').insertAfter('#slider .slider_section:first');
+			slider.css('margin-left', '-100%');
+	}, 800);
+}
+
+function lastToFirst() {
+	$('#slider .slider_section:first').insertAfter('#slider .slider_section:last');
+	$('#slider .slider_section:first').insertBefore('#slider .slider_section:last');
+	slider.animate({
+		marginLeft: 0
+	}, 700);
+	setTimeout(
+		function () {
+			$('#slider .slider_section:first').insertBefore('#slider .slider_section:last');
+			slider.css('margin-left', '-100%');
+	}, 800);
+}
 
 function jumpIn() {
     popUp.animate({
@@ -23,6 +82,7 @@ function jumpIn() {
     }, 500, function (){
 		disabledButtonNext = false;
 		disabledButtonPrev = false;
+		disableIndicator = false;
 	});
 }
 
@@ -56,11 +116,77 @@ function moverPrev() {
 
 function autoPlay() {
 	interval = setInterval(function(){
-		buttonNext.trigger("click");
-		disabledButtonNext = true;
-		disabledButtonPrev = true;
-	}, 4000);
+	buttonNext.trigger("click");
+	}, 5000);
 }
+
+indcator1.on('click', function () {
+	if(disableIndicator == true) return;
+	disableIndicator = true;
+	if (whatIndicator == 1) {
+		buttonPrev.trigger("click");
+	}
+	else{
+		if(whatIndicator == 2){
+				fadeOut();
+				setTimeout(
+					function () {
+						lastToFirst();
+					}, 500);
+				setTimeout(
+					function () {
+						jumpIn();
+						indcator.removeClass("active");
+						whatIndicator = 0;
+						indcator1.addClass("active");
+					}, 1200);
+				clearInterval(interval);
+				autoPlay();
+		}
+		else return;
+	}
+});
+
+indcator2.on('click', function () {
+	if(disableIndicator == true) return;
+	disableIndicator = true;
+	if (whatIndicator == 0) {
+		buttonNext.trigger("click");
+	}
+	else{
+		if(whatIndicator == 2){
+			buttonPrev.trigger("click");
+		}
+		else return;
+	}
+});
+
+indcator3.on('click', function () {
+	if(disableIndicator == true) return;
+	disableIndicator = true;
+	if (whatIndicator == 1) {
+		buttonNext.trigger("click");
+	}
+	else{
+		if(whatIndicator == 0){
+				fadeOut();
+				setTimeout(
+					function () {
+						fisrtToLast();
+					}, 500);
+				setTimeout(
+					function () {
+						jumpIn();
+						indcator.removeClass("active");
+						whatIndicator = 2;
+						indcator3.addClass("active");
+					}, 1200);
+				clearInterval(interval);
+				autoPlay();
+		}
+		else return;
+	}
+});
 
 buttonNext.on('click', function () {
 	if(disabledButtonNext == true) return;
@@ -72,11 +198,13 @@ buttonNext.on('click', function () {
     setTimeout(
         function () {
             jumpIn();
+			nextActive();
         }, 1200);
 	clearInterval(interval);
 	autoPlay();
 	disabledButtonNext = true;
 	disabledButtonPrev = true;
+	disableIndicator = true;
 });
 buttonPrev.on('click', function () {
 	if(disabledButtonPrev == true) return;
@@ -88,6 +216,7 @@ buttonPrev.on('click', function () {
     setTimeout(
         function () {
             jumpIn();
+			prevActive();
         }, 1400);
 	clearInterval(interval);
 	autoPlay();
